0fedc10e2abf7f8538aad58620d7e3cd81683c7b
devshah21 <mail2devshah@gmail.com>
2023-09-16 17:00:06 -0400

    created post_processor for detectron

--- post_processor2.py
+++ post_processor2.py
@@ -1,11 +1,29 @@
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
+import requests
+import numpy as np
+import cv2
 
+from service import *
 
-model = model_zoo.get_model("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-predictor = DefaultPredictor(model)
+input_image = cv2.imread("input.jpg")
 
-# im = cv2.imread("input.jpg")  # Load your input image
-# outputs = predictor(im)
+# Create a dictionary with the image in the format expected by BentoML
+input_data = {'input_img': input_image}
 
+url = 'http://localhost:5000/predict'  # Replace with the actual URL of your service
+response = requests.post(url, files=input_data)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the JSON response
+    result = response.json()
+    
+    # Access the bounding boxes and labels
+    bounding_boxes = result['Boxes']
+    labels = result['Classes']
+    
+    # Now you can use bounding_boxes and labels as needed
+    print("Bounding Boxes:", bounding_boxes)
+    print("Labels:", labels)
+else:
+    print("Error:", response.text)
 
