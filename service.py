c7a48f6b93b2ca10214e82861a99ec168f9b18a1
Arnav Shah <shaharnav03@gmail.com>
2023-09-16 23:05:01 -0400

    centralized repo additions

--- service.py
+++ service.py
@@ -1,13 +1,17 @@
 import bentoml
-from bentoml.io import Image
-from bentoml.io import JSON
-import numpy as np
+from bentoml.io import Text
 from detectron2.utils.logger import setup_logger
 setup_logger()
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from argparse import Namespace
+import base64
+from imageio import imread
+import io
+from post_processor import grid
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
 
 class MaskRCNNRunnable(bentoml.Runnable):
     SUPPORTED_RESOURCES = ("cpu")
@@ -24,25 +28,24 @@ class MaskRCNNRunnable(bentoml.Runnable):
         # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
         self.predictor = DefaultPredictor(cfg)
+        self.cfg = cfg
+
 
     @bentoml.Runnable.method(batchable=False)
     def inference(self, input_img):
-        # Return predictions only
-        print("===INPUT IMAGE===")
-        print(input_img)
-        print("===INPUT TYPE===")
-        print(type(input_img))
-        if isinstance(input_img, np.ndarray):
-            print("===SHAPE===")
-            print(input_img.shape)
-        return {'instances': Namespace(pred_classes=0, pred_boxes=0)}
+        b64str = input_img[0][23:]
+        img = base64.b64decode(b64str)
+        img = imread(io.BytesIO(img))
+        v = Visualizer(img[:,:,::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), scale=1.2)
+        
+        return ". ".join(grid(self.predictor(img), v.metadata.get("thing_classes", None)))
 
 
 mask_rcnn_runner = bentoml.Runner(MaskRCNNRunnable, max_batch_size=30)
 
 svc = bentoml.Service("mask_rcnn_demo", runners=[mask_rcnn_runner])
 
-@svc.api(input=Image(), output=JSON())
+@svc.api(input=Text(), output=Text())
 async def invocation(input_img):
-    batch_ret = await mask_rcnn_runner.inference.async_run([input_img])
-    return {"Boxes": batch_ret['instances'].pred_classes, "Classes": batch_ret['instances'].pred_boxes}
+    strobj = await mask_rcnn_runner.inference.async_run([input_img])
+    return strobj
